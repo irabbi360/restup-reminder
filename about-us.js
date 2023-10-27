6d297af24ffc08ef243d661fec4a6cd763badd95
Fazle Rabbi <35403788+irabbi360@users.noreply.github.com>
2023-10-27 09:51:56 +0600

    about us update

--- about-us.js
+++ about-us.js
@@ -1,19 +1,28 @@
-require('dotenv').config()
-const { ipcRenderer } = require('electron');
-let env = process.env
+const {ipcRenderer, shell } = require('electron');
 
-document.title = `${env.APP_NAME}`;
-document.getElementById('app-name').textContent = `${env.APP_NAME}`;
-document.getElementById('app-version').textContent = `${env.APP_VERSION}`;
-document.getElementById('app-url').textContent = `${env.APP_WEB_URL}`;
+document.addEventListener('DOMContentLoaded', () => {
 
-const closeModalButton = document.getElementById('closeModalButton');
-const closeBtn = document.getElementById('closeBtn');
-closeModalButton.addEventListener('click', () => {
-    // Send a message to the main process to close the modal
-    ipcRenderer.send('close-modal');
-});
-closeBtn.addEventListener('click', () => {
-    // Send a message to the main process to close the modal
-    ipcRenderer.send('close-modal');
+    /*document.title = `${env.APP_NAME}`;
+    document.getElementById('app-name').textContent = `${env.APP_NAME}`;
+    document.getElementById('app-version').textContent = `${env.APP_VERSION}`;
+    document.getElementById('app-url').textContent = `${env.APP_WEB_URL}`;
+*/
+    /*const closeModalButton = document.getElementById('closeModalButton');
+    const closeBtn = document.getElementById('closeBtn');*/
+
+    const websiteBtn = document.getElementById('devWebsite');
+
+   /* closeModalButton.addEventListener('click', () => {
+        // Send a message to the main process to close the modal
+        ipcRenderer.send('close-modal');
+    });
+
+    closeBtn.addEventListener('click', () => {
+        // Send a message to the main process to close the modal
+        ipcRenderer.send('close-modal');
+    });*/
+    websiteBtn.addEventListener('click', () => {
+        shell.openExternal('https://github.com/irabbi360');
+    });
+
 });
